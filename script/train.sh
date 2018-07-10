26e454aaf0b84f99f6898c4a193cbbd59832f702
智慧 <zhihuikaicheng@zhihuideMacBook-Pro.local>
2018-07-10 16:10:32 +0800

    zkc

--- script/train.sh
+++ script/train.sh
@@ -1,5 +1,5 @@
 python NewTrain.py \
---sys_device_ids='(2)' \
+--sys_device_ids='(2,)' \
 --dataset_dir='/world/data-gpu-94/sysu-reid/person-reid-data/Market-1501/Market-1501-v15.09.15/pytorch/train' \
 --model_save_dir='/world/data-gpu-94/sysu-reid/checkpoints/DSR_Market' \
 --img_h=256 \
