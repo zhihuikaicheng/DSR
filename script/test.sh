1239024d6d3e083242e45e1bac435c77195fc08a
智慧 <zhihuikaicheng@zhihuideMacBook-Pro.local>
2018-07-13 16:12:35 +0800

    zkc

--- script/test.sh
+++ script/test.sh
@@ -1,6 +1,8 @@
 python NewTest.py \
 --sys_device_ids='(2,)' \
 --test_dir='/world/data-gpu-94/sysu-reid/person-reid-data/Market-1501/Market-1501-v15.09.15/pytorch' \
+--model_save_dir='/world/data-gpu-94/sysu-reid/checkpoints/DSR_Market' \
 --batch_size=64 \
 --img_h=256 \
---img_w=128
+--img_w=128 \
+--witch_epoch=59
