623b251e8fdcf7d10873be9a6b78a40381a4bad1
智慧 <zhihuikaicheng@zhihuideMacBook-Pro.local>
2018-07-31 17:58:59 +0800

    zkc finetune eval

--- script/eval.sh
+++ script/eval.sh
@@ -1,4 +1,4 @@
 python eval.py \
 --gallery_dir='/world/data-gpu-94/sysu-reid/features_save/DSR_Market/gallery' \
---query_dir='/world/data-gpu-94/sysu-reid/features_save/DSR_Market/query' 
+--query_dir='/world/data-gpu-94/sysu-reid/features_save/DSR_Market/query' \
 --use_multi_scale
