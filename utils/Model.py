2581678b0ddfbfc4331b35e81e23b65628914481
智慧 <zhihuikaicheng@zhihuideMacBook-Pro.local>
2018-08-24 14:06:05 +0800

    zkc renew model

--- utils/Model.py
+++ utils/Model.py
@@ -12,11 +12,12 @@ class Model(nn.Module):
     super(Model, self).__init__()
     # self.base = resnet50(pretrained=True, last_conv_stride=last_conv_stride)
     self.base = models.resnet50(pretrained=True)
-    self.AvgPool1 = nn.AvgPool2d(kernel_size=2, stride=1, padding=0)
-    self.AvgPool2 = nn.AvgPool2d(kernel_size=3, stride=1, padding=0)
-    self.AvgPool3 = nn.AvgPool2d(kernel_size=4, stride=1, padding=0)
-    self.AvgPool4 = nn.AvgPool2d(kernel_size=5, stride=1, padding=0)
-    self.AvgPool5 = nn.AvgPool2d((16, 8), stride=16)
+    self.base.avgpool = nn.AdaptiveAvgPool2d((1,1))
+    # self.AvgPool1 = nn.AvgPool2d(kernel_size=2, stride=1, padding=0)
+    # self.AvgPool2 = nn.AvgPool2d(kernel_size=3, stride=1, padding=0)
+    # self.AvgPool3 = nn.AvgPool2d(kernel_size=4, stride=1, padding=0)
+    # self.AvgPool4 = nn.AvgPool2d(kernel_size=5, stride=1, padding=0)
+    # self.AvgPool5 = nn.AvgPool2d((16, 8), stride=16)
     #self.conv1 = nn.Conv2d(2048, 256, 1, 1, 0)
     # self.fc1 = nn.
     # self.bn1 = nn.BatchNorm2d(2048)
@@ -28,14 +29,14 @@ class Model(nn.Module):
     x = self.base(x)
     feature = x
     #logits = self.fc(feature)
-    x1 = self.AvgPool1(x)
-    x2 = self.AvgPool2(x)
+    # x1 = self.AvgPool1(x)
+    # x2 = self.AvgPool2(x)
     # x3 = self.AvgPool3(x)
     # x4 = self.AvgPool4(x)
     # x5 = self.AvgPool5(x)
-    x = x.view(x.size(0), x.size(1), x.size(2) * x.size(3))
-    x1 = x1.view(x1.size(0), x1.size(1), x1.size(2) * x1.size(3))
-    x2 = x2.view(x2.size(0), x2.size(1), x2.size(2) * x2.size(3))
+    # x = x.view(x.size(0), x.size(1), x.size(2) * x.size(3))
+    # x1 = x1.view(x1.size(0), x1.size(1), x1.size(2) * x1.size(3))
+    # x2 = x2.view(x2.size(0), x2.size(1), x2.size(2) * x2.size(3))
     # x3 = x3.view(x3.size(0), x3.size(1), x3.size(2) * x3.size(3))
     # x4 = x4.view(x4.size(0), x4.size(1), x4.size(2) * x4.size(3))
     # x5 = x5.view(x5.size(0), x5.size(1), x5.size(2) * x5.size(3))
@@ -43,6 +44,7 @@ class Model(nn.Module):
     feature = F.avg_pool2d(feature, feature.size()[2:])
     feature = feature.view(feature.size(0), -1)
 
-    spatialFeature = torch.cat((x, x1, x2), 2)
+    # spatialFeature = torch.cat((x, x1, x2), 2)
 
-    return feature, spatialFeature
+    # return feature, spatialFeature
+    return feature
